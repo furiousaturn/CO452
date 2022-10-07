02d95fe77e1e68dda15de21727762e128e17fd44
furiousaturn <am.stephenson@burnley.ac.uk>
2022-10-07 07:59:18 +0100

    changes

--- Week1/Program.java
+++ Week1/Program.java
@@ -1,11 +1,10 @@
-import java.util.*;
+package Week1;
 
 public class Program
 {
     public static void main(String[] args)
     {
        System.out.println("Hello World");
-       String name;
     
     }
 }
